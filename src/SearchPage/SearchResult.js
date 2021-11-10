280e31bee3375d9aa698f88b2058e00c083e98a3
Tammibriggs <Godsfirstbriggs@gmail.com>
2021-11-10 05:31:54 +0100

    made mobile responsive

--- src/SearchPage/SearchResult.js
+++ src/SearchPage/SearchResult.js
@@ -11,32 +11,32 @@ function SearchResult({
     total,
 }) {
     return (
-        <div className='searchResult'>
-            <img src={img} alt="" />
-            <FavoriteBorder className="searchResult__heart" />
-            <div className='searchResult__info'>
-                <div className="searchResult__infoTop">
-                    <p>{location}</p>
-                    <h3>{title}</h3>
-                    <p>____</p>
-                    <p>{description}</p>
-                </div>
-
-                <div className="searchResult__infoBottom">
-                    <div className="searchResult__stars">
-                        <Star className="searchResult__star" />
-                        <p>
-                            <strong>{star}</strong>
-                        </p>
-                    </div>
-                    <div className='searchResults__price'>
-                        <h2>{price}</h2>
-                        <p>{total}</p>
-                    </div>
-                </div>
-            </div>
+    <div className='searchResult'>
+      <img src={img} alt="" />
+      <FavoriteBorder className="searchResult__heart" />
+      <div className='searchResult__info'>
+        <div className="searchResult__infoTop">
+          <p>{location}</p>
+          <h3>{title}</h3>
+          <p>____</p>
+          <p>{description}</p>
         </div>
-    )
+
+        <div className="searchResult__infoBottom">
+          <div className="searchResult__stars">
+            <Star className="searchResult__star" />
+            <p>
+              <strong>{star}</strong>
+            </p>
+          </div>
+          <div className='searchResults__price'>
+            <h2>{price}</h2>
+            <p>{total}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default SearchResult
--- src/SearchPage/index.js
+++ src/SearchPage/index.js
@@ -14,15 +14,6 @@ function SearchPage() {
                 <Button variant="outlined">Rooms and beds</Button>
                 <Button variant="outlined">More filters</Button>
             </div>
-            <SearchResult
-                img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU"
-                location="Private room in center of London"
-                title="Stay at this spacious Edwardian House"
-                description="1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine"
-                star={4.73}
-                price="£30 / night"
-                total="£117 total"
-            />
 
             <SearchResult
                 img="https://www.expatkings.com/wp-content/uploads/2018/10/Airbnb-rental-tips.-Hostmaker-1-620x349.jpg"
@@ -32,6 +23,16 @@ function SearchPage() {
                 star={4.3}
                 price="£40 / night"
                 total="£157 total"
+            />
+
+            <SearchResult
+                img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU"
+                location="Private room in center of London"
+                title="Stay at this spacious Edwardian House"
+                description="1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine"
+                star={4.73}
+                price="£30 / night"
+                total="£117 total"
             />
 
             <SearchResult
